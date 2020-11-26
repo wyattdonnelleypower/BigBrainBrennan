c3b73ef0c1aca8a8cd21b579a9c17b5adf2aecc4
wyattdonnelleypower <75047064+wyattdonnelleypower@users.noreply.github.com>
2020-11-25 18:41:52 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Security.AccessControl;
+using System.Collections.Generic;
+
+namespace AdventureGameWDP
+{
+    /*    
+    [Big Brain Brennan]
+    [A silly text-based adventure game]
+    [Wyatt Donnelley-Power]
+    [11/03/2020]
+    Uses elements from "Programming Is Fun: C# Code Adventure" by Janell Baxter: http://programmingisfun.com/learn/c-sharp-adventure-game/
+    */
+
+
+    class Program
+    { 
+        static void Main()
+        {
+            
+            Game.StartGame();
+            Game.EndGame();
+
+            Console.ReadKey();
+        }
+    }
+}
